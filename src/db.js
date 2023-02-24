c0794fb22e74baa6e0d05c72a07041b903631703
Al4ex <alexandercruzaragon@gmail.com>
2023-02-24 11:29:37 -0600

    fix start

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () =>{
   try {
       mongoose.set('strictQuery', false)
-      await mongoose.connect(process.env.DB_URL);
+      mongoose.connect(process.env.DB_URL);
       console.log('DB CONECTED');
     } catch (error) {
       handleError(error);
