63963d9ff3626893bf2beef5721d3ebc4405f849
Al4ex <alexandercruzaragon@gmail.com>
2023-02-24 12:41:58 -0600

    change version apollo server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import bodyParser from 'body-parser';
+import cors from 'cors'
+import http from 'http'
 import "dotenv/config";
 import resolvers from './resolvers';
 import typeDefs from './typeDef';
@@ -9,6 +13,7 @@ import connectDB from './db';
 const init = async () =>{
     // Configuración de Express
 const app = express();
+const httpServer = http.createServer(app);
 connectDB()
 
 // Configuración de Apollo Server
@@ -16,20 +21,28 @@ connectDB()
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+
 await server.start()
-server.applyMiddleware({ app });
 
 // Configuración de Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(
+  '/graphql',
+  cors(),
+  express.json(),
+  expressMiddleware(server),
+);
 
 // Arranque del servidor
 const port = process.env.PORT || 3000;;
+await new Promise((resolve) => httpServer.listen({ port }, resolve));
 
-app.listen(port, () => {
-  console.log(`Servidor levantado en el puerto ${port}.`);
-});
+console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
+
+// app.listen(port, () => {
+// });
 }
 init()
