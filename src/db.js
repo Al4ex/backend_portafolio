bcfcccc5aa928d64efc0b25b61154503b925134d
Al4ex <alexandercruzaragon@gmail.com>
2023-02-24 13:52:05 -0600

    add config connect

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () =>{
   try {
       mongoose.set('strictQuery', false)
-      mongoose.connect(process.env.DB_URL);
+      mongoose.connect(process.env.DB_URL,{useNewUrlParser: true, useUnifiedTopology: true});
       console.log('DB CONECTED');
     } catch (error) {
       handleError(error);
